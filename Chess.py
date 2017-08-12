47d9a32eabba7bbb84644d2bff6b9dd5ac7c0fe7
let-robots-reign <zotov.alexei.1@yandex.ru>
2017-08-12 18:16:37 +0400

    Added figures. Defined classes: Pawn, Rook, Knight, Bishop, Queen, King.

--- Chess.py
+++ Chess.py
@@ -65,6 +65,22 @@ class Board:
         self.field = []
         for row in range(8):
             self.field.append([None] * 8)
+        self.field[0] = [
+            Rook(WHITE), Knight(WHITE), Bishop(WHITE), Queen(WHITE),
+            King(WHITE), Bishop(WHITE), Knight(WHITE), Rook(WHITE)
+        ]
+        self.field[1] = [
+            Pawn(WHITE), Pawn(WHITE), Pawn(WHITE), Pawn(WHITE),
+            Pawn(WHITE), Pawn(WHITE), Pawn(WHITE), Pawn(WHITE)
+        ]
+        self.field[6] = [
+            Pawn(BLACK), Pawn(BLACK), Pawn(BLACK), Pawn(BLACK),
+            Pawn(BLACK), Pawn(BLACK), Pawn(BLACK), Pawn(BLACK)
+        ]
+        self.field[7] = [
+            Rook(BLACK), Knight(BLACK), Bishop(BLACK), Queen(BLACK),
+            King(BLACK), Bishop(BLACK), Knight(BLACK), Rook(BLACK)
+        ]
 
     def current_player_color(self):
         return self.color
@@ -82,6 +98,12 @@ class Board:
         c = 'w' if color == WHITE else 'b'
         return c + piece.char()
 
+    def get_piece(self, row, col):
+        if correct_coords(row, col):
+            return self.field[row][col]
+        else:
+            return None
+
     def move_piece(self, row, col, row1, col1):
         """
         Переместить фигуру из точки (row, col) в точку (row1, col1).
@@ -91,19 +113,167 @@ class Board:
         if not correct_coords(row, col) or not correct_coords(row1, col1):
             return False
         if row == row1 and col == col1:
-            return False  # Нельзя пойти в ту же клетку
+            return False  # нельзя пойти в ту же клетку
         piece = self.field[row][col]
         if piece is None:
             return False
         if piece.get_color() != self.color:
             return False
-        if not piece.can_move(row1, col1):
+        if self.field[row1][col1] is None:
+            if not piece.can_move(self, row, col, row1, col1):
+                return False
+        elif self.field[row1][col1].get_color() == opponent(piece.get_color()):
+            if not piece.can_attack(self, row, col, row1, col1):
+                return False
+        else:
             return False
         self.field[row][col] = None  # Снять фигуру.
         self.field[row1][col1] = piece  # Поставить на новое место.
-        piece.set_position(row1, col1)
         self.color = opponent(self.color)
         return True
+
+
+class Pawn:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return 'P'
+
+    def can_move(self, board, row, col, row1, col1):
+        # Пешка может ходить только по вертикали
+        # "взятие на проходе" не реализовано
+        if col != col1:
+            return False
+
+        # Пешка может сделать из начального положения ход на 2 клетки
+        # вперёд, поэтому поместим индекс начального ряда в start_row.
+        if self.color == WHITE:
+            direction = 1
+            start_row = 1
+        else:
+            direction = -1
+            start_row = 6
+
+        # ход на 1 клетку
+        if row + direction == row1:
+            return True
+
+        # ход на 2 клетки из начального положения
+        if row == start_row and row + 2 * direction == row1 and board.field[row + direction][col] is None:
+            return True
+
+        return False
+
+    def can_attack(self, row, col, row1, col1):
+        direction = 1 if (self.color == WHITE) else -1
+        return row + direction == row1 and (col + 1 == col1 or col - 1 == col1)
+
+
+class Rook:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return 'R'
+
+    def can_move(self, board, row, col, row1, col1):
+        # Невозможно сделать ход в клетку, которая не лежит в том же ряду
+        # или столбце клеток.
+        if row != row1 and col != col1:
+            return False
+
+        step = 1 if (row1 >= row) else -1
+        for r in range(row + step, row1, step):
+            # Если на пути по вертикали есть фигура
+            if not (board.get_piece(r, col) is None):
+                return False
+
+        step = 1 if (col1 >= col) else -1
+        for c in range(col + step, col1, step):
+            # Если на пути по горизонтали есть фигура
+            if not (board.get_piece(row, c) is None):
+                return False
+
+        return True
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+
+class Knight:
+    """
+    Класс коня. Пока что заглушка, которая может ходить в любую клетку.
+    """
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return 'N'  # kNight, буква 'K' уже занята королём
+
+    def can_move(self, board, row, col, row1, col1):
+        return True  # Заглушка
+
+
+class King:
+    """
+    Класс короля. Пока что заглушка, которая может ходить в любую клетку.
+    """
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return 'K'
+
+    def can_move(self, board, row, col, row1, col1):
+        return True  # Заглушка
+
+
+class Queen:
+    """
+    Класс ферзя. Пока что заглушка, которая может ходить в любую клетку.
+    """
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return 'Q'
+
+    def can_move(self, board, row, col, row1, col1):
+        return True  # Заглушка
+
+
+class Bishop:
+    """
+    Класс слона. Пока что заглушка, которая может ходить в любую клетку.
+    """
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return 'B'
+
+    def can_move(self, board, row, col, row1, col1):
+        return True  # Заглушка
+
 
 if __name__ == "__main__":
     main()
